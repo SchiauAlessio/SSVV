3b6347eee967d7b07ef91bea748776f359a8742a
SchiauAlessio <alessio.schiau1@gmail.com>
2023-04-06 16:22:56 +0300

    add addTema tests

---
+++ src/test/java/ssvv/example/AssignmentTest.java
@@ -0,0 +1,51 @@
+package ssvv.example;
+
+import org.junit.Before;
+import org.junit.Test;
+import ssvv.example.domain.Tema;
+import ssvv.example.repository.NotaXMLRepo;
+import ssvv.example.repository.StudentXMLRepo;
+import ssvv.example.repository.TemaXMLRepo;
+import ssvv.example.service.Service;
+import ssvv.example.validation.NotaValidator;
+import ssvv.example.validation.StudentValidator;
+import ssvv.example.validation.TemaValidator;
+import ssvv.example.validation.ValidationException;
+
+import java.io.File;
+import java.io.FileWriter;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+public class AssignmentTest {
+    private Service service;
+    private TemaXMLRepo temaRepo;
+
+    @Before
+    public void setUp() throws Exception {
+        File file = new File("src/test/java/ssvv/example/files/testTeme.xml");
+        FileWriter fileWriter = new FileWriter(file);
+        fileWriter.write("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>\n<inbox>\n</inbox>");
+        fileWriter.close();
+        StudentXMLRepo studentRepo = new StudentXMLRepo("src/test/java/ssvv/example/files/testStudenti.xml");
+        temaRepo = new TemaXMLRepo("src/test/java/ssvv/example/files/testTeme.xml");
+        NotaXMLRepo notaRepo = new NotaXMLRepo("src/test/java/ssvv/example/files/testNote.xml");
+        service = new Service(studentRepo, new StudentValidator(), temaRepo, new TemaValidator(), notaRepo, new NotaValidator(studentRepo, temaRepo));
+    }
+
+    @Test
+    public void addAssignmentShouldAddValidAssignmentToRepository() {
+        Tema tema = new Tema("1", "Description", 1, 1);
+        Tema result = service.addTema(tema);
+        assertNull(result);
+        assertEquals(tema, temaRepo.findOne("1"));
+    }
+
+    @Test(expected = ValidationException.class)
+    public void addAssignmentShouldThrowValidationExceptionForNegativePrimire() {
+        Tema tema = new Tema("1", "Description", 1, -1);
+        service.addTema(tema);
+    }
+
+}
