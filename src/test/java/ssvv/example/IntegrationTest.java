4a8401db1b159b5049e3efdb9e58e1143fac6967
SchiauAlessio <alessio.schiau1@gmail.com>
2023-05-03 18:30:39 +0300

    added incremental integration

--- src/test/java/ssvv/example/IntegrationTest.java
+++ src/test/java/ssvv/example/IntegrationTest.java
@@ -18,8 +18,9 @@ import java.io.File;
 import java.io.FileWriter;
 import java.time.LocalDate;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 public class IntegrationTest {
     private Service service;
@@ -52,6 +53,24 @@ public class IntegrationTest {
         service = new Service(studentRepo, new StudentValidator(), temaRepo, new TemaValidator(), notaRepo, new NotaValidator(studentRepo, temaRepo));
     }
 
+    private Service createMockService() {
+        Student student = new Student("1", "John", 932, "em@em.em");
+        Tema tema = new Tema("1", "Description", 13, 13);
+
+        StudentXMLRepo mockStudentRepo = Mockito.mock(StudentXMLRepo.class);
+        TemaXMLRepo mockTemaRepo = Mockito.mock(TemaXMLRepo.class);
+        
+        when(mockStudentRepo.save(any(Student.class))).thenReturn(null);
+        when(mockStudentRepo.findOne("1")).thenReturn(student);
+
+        when(mockTemaRepo.save(any(Tema.class))).thenReturn(null);
+        when(mockTemaRepo.findOne("1")).thenReturn(tema);
+
+        // Create Service with mock repos
+
+        return new Service(mockStudentRepo, new StudentValidator(), mockTemaRepo, new TemaValidator(), notaRepo, new NotaValidator(mockStudentRepo, mockTemaRepo));
+    }
+
     @Test
     public void addValidStudentShouldAddStudentToRepository() {
         Student student = new Student("1", "John", 932, "student@gmail.com");
@@ -77,8 +96,8 @@ public class IntegrationTest {
         // Set up mock behavior
         Student student = new Student("1", "John", 932, "student@gmail.com");
         Tema tema = new Tema("1", "Description", 13, 13);
-        Mockito.when(mockStudentRepo.findOne("1")).thenReturn(student);
-        Mockito.when(mockTemaRepo.findOne("1")).thenReturn(tema);
+        when(mockStudentRepo.findOne("1")).thenReturn(student);
+        when(mockTemaRepo.findOne("1")).thenReturn(tema);
 
         // Create Service with mock repos
         Service mockService = new Service(mockStudentRepo, new StudentValidator(), mockTemaRepo, new TemaValidator(), notaRepo, new NotaValidator(mockStudentRepo, mockTemaRepo));
@@ -103,5 +122,43 @@ public class IntegrationTest {
 
         assertEquals(nota, notaRepo.findOne("1"));
         assertEquals(10.0, result3, 0.01);
+    }
+
+    @Test
+    public void testAddStudentValid() {
+        Student student = new Student("1", "John", 932, "student@gmail.com");
+
+        Service mockService = createMockService();
+
+        Student addedStudent = mockService.addStudent(student);
+
+        assertNull(addedStudent);
+    }
+
+    @Test
+    public void testAddStudent_AddAssignment() {
+        Service service = createMockService();
+        Student student = new Student("1", "John Doe", 937, "st@gmail.com");
+        service.addStudent(student);
+
+        Tema tema = new Tema("1", "Assignment description", 4, 2);
+        Tema addedTema = service.addTema(tema);
+
+        assertNull(addedTema);
+    }
+
+    @Test
+    public void testAddStudent_AddAssignment_AddGrade() {
+        Service service = createMockService();
+
+        Student student = new Student("1", "John Doe", 937, "email@gmail");
+        service.addStudent(student);
+
+        Tema tema = new Tema("1", "Assignment description", 14, 14);
+        service.addTema(tema);
+
+        Nota nota = new Nota("1", "1", "1", 10D, LocalDate.now());
+        double result = service.addNota(nota, "Good job!");
+        assertEquals(10.0, result, 0.01);
     }
 }
